ec84ef4816f82229ed64204883d214a181b095f6
DGenov <dgenov@tu-sofia.bg>
2024-04-08 15:35:44 +0300

    completed exercise one

---
+++ PS_42_Dimitar_Genov/Welcome/Program.cs
@@ -0,0 +1,23 @@
+﻿using Welcome.Model;
+using Welcome.View;
+using Welcome.ViewModel;
+
+namespace Welcome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+
+            user.Name = "Test";
+            user.Password = "test password";
+            user.Role = Others.UserRolesEnum.ADMIN;
+
+            UserViewModel userViewModel = new UserViewModel(user);
+
+            UserView userView = new UserView(userViewModel);
+            userView.Display();
+        }
+    }
+}
