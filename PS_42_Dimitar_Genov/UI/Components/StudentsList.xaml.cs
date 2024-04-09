23cdb7cbe2c94d9c767ba13afdc6f73ffa41d31c
Dimitar Genov <dgenov@tu-sofia.bg>
2024-04-10 00:08:48 +0300

    Exercise 5 completed

---
+++ PS_42_Dimitar_Genov/UI/Components/StudentsList.xaml.cs
@@ -0,0 +1,36 @@
+﻿using DataLayer.Database;
+using DataLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Welcome.Others;
+
+namespace UI.Components
+{
+    /// <summary>
+    /// Interaction logic for StudentsList.xaml
+    /// </summary>
+    public partial class StudentsList : UserControl
+    {
+        public StudentsList()
+        {
+            InitializeComponent();
+            using (var context = new DatabaseContext())
+            {     
+                var users = context.Users.ToList();
+                students.DataContext = users;
+            }
+        }
+    }
+}
