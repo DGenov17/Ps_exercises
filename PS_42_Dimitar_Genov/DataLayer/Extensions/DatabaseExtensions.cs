dfca1f5b2a84552337c4182747dd2a9beb064ed2
Dimitar Genov <dgenov@tu-sofia.bg>
2024-04-09 23:28:01 +0300

    exercise 4 completed

---
+++ PS_42_Dimitar_Genov/DataLayer/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,28 @@
+﻿using DataLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Extensions
+{
+    public static class DatabaseExtensions
+    {
+        public static void CheckValidUser(this List<DatabaseUser> databaseUsers, string name, string password)
+        {
+            var result = from user in databaseUsers
+                         where user.Name == name && user.Password == password
+                         select user;
+
+            if (result.Any())
+            {
+                Console.WriteLine("Valid User");
+            }
+            else
+            {
+                Console.WriteLine("User Not Valid");
+            }
+        }
+    }
+}
